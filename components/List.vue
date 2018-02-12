12c52fab33ab1a7a5efc5e40395c384ef28a5d5d
wesssel <wessel.vanderpal@vergleich.org>
2018-02-12 16:42:46 +0900

    init

---
+++ components/List.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in list" :key="index">
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'list',
+
+    props: {
+      list: {
+        type: Array,
+        required: true,
+      },
+    },
+  }
+</script>
---
+++ test/specs/List.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+import { mount } from '@vue/test-utils'
+import List from '../../components/List'
+
+let wrapper
+
+const list = ['Coffee', 'Tea', 'Snicker', 'Mars']
+
+test.beforeEach(() => {
+  wrapper = mount(List, {
+    propsData: { list },
+  })
+})
+
+test('List.vue to display list', (t) => {
+  const $items = wrapper.findAll('li').wrappers
+
+  $items.forEach(($item, index) => {
+    t.is($item.text(), list[index])
+  })
+})
